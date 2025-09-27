4f2f7d9e36eed249e9261e5af1f4903ffa196668
nautore <nautore@proton.me>
2025-09-27 11:02:08 -0700

    Added new enemy type

--- 2d-top-down-shooter-tutorial/scripts/game.gd
+++ 2d-top-down-shooter-tutorial/scripts/game.gd
@@ -1,15 +1,22 @@
 extends Node2D
 
-var enemy_scene = preload("res://scenes/enemy.tscn")
+var enemy_scene_1 = preload("res://scenes/enemy.tscn")
+var enemy_scene_2 = preload("res://scenes/enemy2.tscn")
 
 @onready var player = $Player
 
 func _on_timer_timeout() -> void:
-	var enemy = enemy_scene.instantiate()
+	var enemy = enemy_scene_1.instantiate()
+	var enemy2 = enemy_scene_2.instantiate()
 	enemy.global_position = player.global_position
+	enemy2.global_position = player.global_position
 	
 	while enemy.global_position.distance_squared_to(player.global_position) < 200000:
 		enemy.global_position.x = randi_range(0, get_viewport_rect().size.x)
 		enemy.global_position.y = randi_range(0, get_viewport_rect().size.y)
+	while enemy2.global_position.distance_squared_to(player.global_position) < 200000:
+		enemy2.global_position.x = randi_range(0, get_viewport_rect().size.x)
+		enemy2.global_position.y = randi_range(0, get_viewport_rect().size.y)
 		
 	add_child(enemy)
+	add_child(enemy2)
